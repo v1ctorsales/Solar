06fd6ca7fa7d80c889df24621133ad0091c43c49
Victor <victor.alves.sales@hotmail.com>
2023-03-18 23:29:41 -0300

    sol teste

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//editar o height
+document.querySelector(".txtsol").addEventListener("mouseover",()=>handleHover())
+document.querySelector(".txtsol").addEventListener("mouseout",()=>mouseFora())
+
+function mouseFora(){
+    for(x=0;x<8;x++){
+        document.querySelectorAll(".orbita img")[x].classList.add("visivel");
+    }
+}
+
+function handleHover(){
+    for(x=0;x<8;x++){
+    document.querySelectorAll(".orbita img")[x].classList.add("invisivel");
+}
+}
