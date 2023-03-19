9de0aca94776af73f2a6600e7d49785641dbdcd0
Victor <victor.alves.sales@hotmail.com>
2023-03-18 23:56:50 -0300

    mudancas

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
-document.querySelector(".txtsol").addEventListener("mouseover",()=>handleHover())
-document.querySelector(".txtsol").addEventListener("mouseout",()=>mouseFora())
+document.querySelectorAll(".navul li")[0].addEventListener("mouseover",()=>handleHoverSOL())
+document.querySelectorAll(".navul li")[0].addEventListener("mouseout",()=>mouseForaSOL())
 
-function mouseFora(){
+function mouseForaSOL(){
     for(x=0;x<8;x++){
-        document.querySelectorAll(".orbita img")[x].classList.add("visivel");
+        document.querySelectorAll(".orbita img")[x].classList.remove("invisivel");
     }
 }
 
-function handleHover(){
+function handleHoverSOL(){
     for(x=0;x<8;x++){
+    document.querySelectorAll(".orbita img")[x].classList.add("invisivel");
+}
+}
+
+// Mercurio
+
+document.querySelectorAll(".navul li")[1].addEventListener("mouseover",()=>handleHoverMER())
+document.querySelectorAll(".navul li")[1].addEventListener("mouseout",()=>mouseForaMER())
+
+function mouseForaMER(){
+    for(x=1;x<8;x++){
+        document.querySelectorAll(".orbita img")[x].classList.remove("invisivel");
+    }
+}
+
+function handleHoverMER(){
+    for(x=1;x<8;x++){
+    document.querySelectorAll(".orbita img")[x].classList.add("invisivel");
+}
+}
+
+// Venus
+
+document.querySelectorAll(".navul li")[2].addEventListener("mouseover",()=>handleHoverVEN())
+document.querySelectorAll(".navul li")[2].addEventListener("mouseout",()=>mouseForaVEN())
+
+function mouseForaVEN(){
+    document.querySelectorAll(".orbita img")[0].classList.remove("invisivel");
+    for(x=2;x<8;x++){
+        document.querySelectorAll(".orbita img")[x].classList.remove("invisivel");
+    }
+}
+
+function handleHoverVEN(){
+    document.querySelectorAll(".orbita img")[0].classList.add("invisivel");
+    for(x=2;x<8;x++){
+    document.querySelectorAll(".orbita img")[x].classList.add("invisivel");
+}
+}
+
+// Terra
+
+document.querySelectorAll(".navul li")[3].addEventListener("mouseover",()=>handleHoverTER())
+document.querySelectorAll(".navul li")[3].addEventListener("mouseout",()=>mouseForaTER())
+
+function mouseForaTER(){
+    document.querySelectorAll(".orbita img")[0].classList.add("invisivel");
+    document.querySelectorAll(".orbita img")[1].classList.remove("invisivel");
+    for(x=3;x<8;x++){
+        document.querySelectorAll(".orbita img")[x].classList.remove("invisivel");
+    }
+}
+
+function handleHoverTER(){
+    document.querySelectorAll(".orbita img")[0].classList.add("invisivel");
+    document.querySelectorAll(".orbita img")[1].classList.add("invisivel");
+    for(x=3;x<8;x++){
     document.querySelectorAll(".orbita img")[x].classList.add("invisivel");
 }
 }
